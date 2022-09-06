14f587538429f187b45e310c8bea476d41d5b07e
Mehedi <mehedi@augnitive.com>
2022-09-06 19:41:14 +0600

    main screen added

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:recipe/ui/colors.dart';
 import 'package:recipe/ui/myrecipes/my_recipe_list.dart';
 import 'package:recipe/ui/recipes/recipe_list.dart';
 import 'package:recipe/ui/shopping/shopping_list.dart';
@@ -24,8 +27,86 @@ class _MainScreenState extends State<MainScreen> {
     super.initState();
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // TODO: Call saveCurrentIndex
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    String title;
+    switch (_selectedIndex) {
+      case 0:
+        title = 'Recipes';
+        break;
+      case 1:
+        title = 'Bookmarks';
+        break;
+      case 2:
+        title = 'Groceries';
+        break;
+      default:
+        title = 'Recipes';
+        break;
+    }
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/images/icon_recipe.svg',
+                color: _selectedIndex == 0 ? green : Colors.grey,
+                semanticsLabel: 'Recipes',
+              ),
+              label: 'Recipes'),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/images/icon_bookmarks.svg',
+              color: _selectedIndex == 1 ? green : Colors.grey,
+              semanticsLabel: 'Bookmarks',
+            ),
+            label: 'Bookmarks',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/images/icon_shopping_list.svg',
+              color: _selectedIndex == 2 ? green : Colors.grey,
+              semanticsLabel: 'Groceries',
+            ),
+            label: 'Groceries',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: green,
+        onTap: _onItemTapped,
+      ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          systemNavigationBarColor: Colors.white,
+          statusBarColor: Colors.white,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.dark,
+          systemNavigationBarDividerColor: Colors.white,
+          // Navigation bar divider color
+          systemNavigationBarIconBrightness: Brightness.light,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: pageList,
+      ),
+    );
   }
 }
