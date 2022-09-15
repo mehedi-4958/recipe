8b047e386bbe12915e10a397b5235fe6df53e0ab
Mehedi <mehedi@augnitive.com>
2022-09-15 22:38:05 +0600

    few changes

---
+++ lib/data/drift/drift_db.dart
@@ -0,0 +1,17 @@
+part 'drift_db.g.dart';
+
+class DrfitRecipe extends Table
+
+// TODO: Add DriftIngredient table definition here
+
+// TODO: Add @UserMoor() adn RecipeDatabase() here
+
+// TODO Add RecipeDao here
+
+// TODO: Add IngredientDao
+
+// TODO: Add driftRecipeTORecipe here
+
+// TODO: Add driftRecipeData here
+
+// TODO: Add moorIngredientToIngredient and MoorIngredientCompanion here
